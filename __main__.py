2fdf0b20a633f2157d6435ccce87b0152e5cdb1b
Arthur Caillaud <arthur@Arthur-Caillaud.local>
2018-10-07 19:42:17 +0200

    Adding some methods for Array

--- __main__.py
+++ __main__.py
@@ -1,8 +1,3 @@
 from serpyan import Array
 
-a = [1,2,3,4,5,6]
-b = Array(a)
-b.map(lambda x: 2*x).map(lambda x: x+1).filter(lambda x: x%3 == 0)
-
-print(b)
-print(a)
+print (Array.range(10000).filter(lambda x : x % 3 == 0).reduce(lambda acc, x : acc + x, 0))
--- serpyan.py
+++ serpyan.py
@@ -1,3 +1,5 @@
+import math
+
 class Array:
     def __init__(self, array):
         self.__array = array[::]
@@ -8,12 +10,35 @@ class Array:
     def __str__(self):
         return self.__array.__str__()
 
+    @classmethod
+    def range(cls, int):
+        arr = []
+        for k in range(int):
+            arr.append(k)
+        return cls(arr)
+
+    @classmethod
+    def empty(cls, int):
+        arr = []
+        for k in range(int):
+            arr.append(None)
+        return cls(arr)
+
+    def each(self, function):
+        new_array = []
+        for k in range(len(self.__array)):
+            el = self.__array[k]
+            new_array.append(el)
+            function(el)
+        return Array(new_array)
+
     def map(self, function):
+        new_array = []
         for k in range(len(self.__array)):
             el = self.__array[k]
             new_el = function(el)
-            self.__array[k] = new_el
-        return self
+            new_array.append(new_el)
+        return Array(new_array)
 
     def filter(self, function):
         new_array = []
@@ -21,8 +46,72 @@ class Array:
             el = self.__array[k]
             if (function(el)):
                 new_array.append(el)
-        self.__array = new_array
-        return self
+        return Array(new_array)
 
-    def reduce(self, function):
-        return 0
+    def reject(self, function):
+        new_array = []
+        for k in range(len(self.__array)):
+            el = self.__array[k]
+            if (not function(el)):
+                new_array.append(el)
+        return Array(new_array)
+
+    def reduce(self, function, initialValue):
+        acc = initialValue
+        for k in range(len(self.__array)):
+            el = self.__array[k]
+            acc = function(acc, el)
+        return acc
+
+    def find(self, function):
+        for k in range(len(self.__array)):
+            el = self.__array[k]
+            if (function(el)):
+                return el
+        return None
+
+    def find_index(self, function):
+        for k in range(len(self.__array)):
+            el = self.array[k]
+            if (function(el)):
+                return k
+        return -1
+
+    def every(self, function):
+        for k in range(len(self.__array)):
+            el = self.array[k]
+            if (not function(el)):
+                return False
+        return True
+
+    def some(self, function):
+        for k in range(len(self.__array)):
+            el = self.__array[k]
+            if (function(el)):
+                return True
+        return False
+
+    def contains(self, value):
+        for k in range(len(self.__array)):
+            el = self.__array[k]
+            if (el == value):
+                return True
+        return False
+
+    def max(self, function):
+        max = -math.inf
+        for k in range(len(self.__array)):
+            el = self.__array[k]
+            score = function(el)
+            if (score > max):
+                max = score
+        return max
+
+    def min(self, function):
+        min = math.inf
+        for k in range(len(self.__array)):
+            el = self.__array[k]
+            score = function(el)
+            if (score < min):
+                min = score
+        return min
